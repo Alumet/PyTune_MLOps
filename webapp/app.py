a0c75ab7258bff0192fbe13d2fdcb5d1e7ad8cd7
yann.huet <yhuet.yann@gmail.com>
2023-02-01 17:49:39 +0100

    webapp refactor

--- webapp/app.py
+++ webapp/app.py
@@ -4,90 +4,119 @@ from youtubesearchpython import VideosSearch
 import dotenv
 import os
 from random import shuffle
+from typing import Tuple, List
 
+# Load .env
 dotenv.load_dotenv()
+api_url = os.getenv('API_URL')
 
 st.set_page_config(layout="wide")
 
-if 'video_url' not in st.session_state:
-    st.session_state.video_url = None
-if 'search' not in st.session_state:
-    st.session_state.search = None
-if 'listened_tracks' not in st.session_state:
-    st.session_state.listened_tracks = list()
 
-api_url = os.getenv('API_URL')
-
-
-def log_is_valid() -> bool:
-    x = requests.get(f'{api_url}/login',
-                     auth=(st.session_state.username, st.session_state.password))
-    return x.status_code == 200
-
-
-def add_user(user_name: str, pass_word: str) -> bool:
-    data = {"user_name": user_name, "pass_word": pass_word, "is_admin": False}
-    x = requests.post(f'{api_url}/admin/user', json=data,
-                      auth=('admin', 'admin'))
-
-    return x.status_code == 200
-
-
-def update_reco() -> None:
+# Set Up Session
+def set_session():
+    if 'video_url' not in st.session_state:
+        st.session_state.video_url = None
+    if 'search' not in st.session_state:
+        st.session_state.search = None
+    if 'listened_tracks' not in st.session_state:
+        st.session_state.listened_tracks = list()
+    if 'logged' not in st.session_state:
+        st.session_state.logged = False
+    if 'warning' not in st.session_state:
+        st.session_state.warning = ''
     if 'track_list' not in st.session_state:
         st.session_state.track_list = list()
 
-    n_track = 20 - len(st.session_state.track_list)
 
-    if n_track > 10:
+def reset_session():
+    st.session_state.logged = False
+    st.session_state.track_list = list()
+    st.session_state.video_url = None
+    st.session_state.listened_tracks = list()
+
+
+set_session()
+
+
+# Class to communicate with API
+class API:
+
+    @staticmethod
+    def log_is_valid() -> bool:
+        x = requests.get(f'{api_url}/login',
+                         auth=(st.session_state.username, st.session_state.password))
+        return x.status_code == 200
+
+    @staticmethod
+    def add_user(user_name: str, pass_word: str) -> bool:
+        data = {"user_name": user_name, "pass_word": pass_word, "is_admin": False}
+        x = requests.post(f'{api_url}/admin/user', json=data,
+                          auth=('admin', 'admin'))
+
+        return x.status_code == 200
+
+    @staticmethod
+    def reco_model(n_track: int) -> List:
         data = {"N_track": n_track, "filter_already_liked": True}
         x = requests.post(f'{api_url}/recommendation', json=data,
                           auth=(st.session_state.username, st.session_state.password))
-        if x.status_code != 200:
-            if st.session_state.listened_tracks:
-                data = {"N_track": 3, 'track_id': st.session_state.listened_tracks[-1]}
-                x = requests.post(f'{api_url}/similar', json=data,
-                                  auth=(st.session_state.username, st.session_state.password))
-                reco = x.json()
-            else:
-                reco = dict()
-        else:
+        if x.status_code == 200:
             reco = x.json()
-    else:
-        reco = dict()
+            return [reco.get(key) for key in reco]
+        else:
+            return []
 
-    for key in reco:
-        track = reco.get(key)
-        if track not in st.session_state.track_list:
-            st.session_state.track_list.append(reco.get(key))
+    @staticmethod
+    def reco_similar() -> List:
+        reco = list()
+        for i, id in enumerate(st.session_state.listened_tracks[::-1]):
+            data = {"N_track": 2, 'track_id': id}
+            x = requests.post(f'{api_url}/similar', json=data,
+                              auth=(st.session_state.username, st.session_state.password))
+            if x.status_code == 200:
+                sub_reco = x.json()
+            else:
+                sub_reco = []
 
-    shuffle(st.session_state.track_list)
+            if i > 2:
+                break
+
+            reco += [sub_reco.get(key) for key in sub_reco]
+            shuffle(reco)
+
+        return reco
+
+    @staticmethod
+    def add_event(track) -> None:
+        data = {"track_id": track.get('track_id')}
+        x = requests.post(f'{api_url}/event', json=data,
+                          auth=(st.session_state.username, st.session_state.password))
+
+    @staticmethod
+    def track_search(txt):
+        data = {"search": txt}
+        x = requests.post(f'{api_url}/search', params=data,
+                          auth=(st.session_state.username, st.session_state.password))
+        ans = x.json()
+
+        st.session_state.search_result = [ans.get(key) for key in ans]
 
 
-def add_event(track) -> None:
+def youtube_search(txt: str) -> Tuple[str, str]:
+    videos_search = VideosSearch(txt, limit=1)
 
-    data = {"track_id": track.get('track_id')}
-    x = requests.post(f'{api_url}/event', json=data,
-                      auth=(st.session_state.username, st.session_state.password))
+    url = videos_search.result().get('result')[0].get('link')
+    length = videos_search.result().get('result')[0].get('duration')
 
-
-def track_search(txt):
-    data = {"search": txt}
-    x = requests.post(f'{api_url}/search', params=data,
-                      auth=(st.session_state.username, st.session_state.password))
-    ans = x.json()
-
-    st.session_state.search_result = [ans.get(key) for key in ans]
+    return url, length
 
 
 def play(track: dict, id: int, remove: bool = False) -> None:
     st.session_state.playing = track
 
     txt = track.get('artist_name') + ' ' + track.get('track_name')
-    videos_search = VideosSearch(txt, limit=1)
-
-    url = videos_search.result().get('result')[0].get('link')
-    length = videos_search.result().get('result')[0].get('duration')
+    url, length = youtube_search(txt)
 
     if remove:
         st.session_state.track_list.pop(id)
@@ -95,48 +124,56 @@ def play(track: dict, id: int, remove: bool = False) -> None:
     st.session_state.video_url = url
     st.session_state.video_length = length
 
-    add_event(track)
+    API.add_event(track)
     st.session_state.listened_tracks.append(track.get('track_id'))
 
 
-with st.sidebar:
-    st.title('Pytune')
+def update_reco() -> None:
+    reco = API.reco_model(50)
 
-    if 'logged' not in st.session_state:
-        st.session_state.logged = False
+    if reco == [] and len(st.session_state.listened_tracks) > 0:
+        reco = API.reco_similar()
 
-    if not st.session_state.logged:
-        st.subheader('Sign in')
-        st.session_state.username = st.text_input('username', value='username', label_visibility="collapsed")
-        st.session_state.password = st.text_input('password', value='password', label_visibility="collapsed",
-                                                  type="password")
+    for track in reco.copy():
+        if track.get('track_id') in st.session_state.listened_tracks:
+            reco.remove(track)
 
-        col1, col2 = st.columns(2)
+    st.session_state.track_list = reco
 
-        with col1:
-            if st.button('Logg in'):
-                if log_is_valid():
-                    st.session_state.logged = True
-                    st.experimental_rerun()
-
-        with col2:
-            if st.button('New user'):
-                if add_user(st.session_state.username, st.session_state.password):
-                    st.session_state.logged = True
-                    st.experimental_rerun()
-
-    else:
-        st.write('User logged as: ', st.session_state.username)
-        if st.button('Log out'):
-            st.session_state.logged = False
-            st.session_state.track_list = list()
-            st.session_state.video_url = None
-            st.session_state.listened_tracks = list()
-            st.experimental_rerun()
+    st.session_state.listened_tracks = st.session_state.listened_tracks[:20]
 
 
-if st.session_state.logged:
+def login_widget():
+    st.subheader('Sign in')
 
+    st.session_state.username = st.text_input('username', value='username', label_visibility="collapsed")
+    st.session_state.password = st.text_input('password', value='password', label_visibility="collapsed",
+                                              type="password")
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        if st.button('Logg in'):
+            if API.log_is_valid():
+                st.session_state.logged = True
+                st.experimental_rerun()
+                st.session_state.warning = ""
+            else:
+                st.session_state.warning = "!! Invalid username or password !!"
+
+    with col2:
+        if st.button('New user'):
+            if API.add_user(st.session_state.username, st.session_state.password):
+                st.session_state.logged = True
+                st.experimental_rerun()
+                st.session_state.warning = ""
+            else:
+                st.session_state.warning = " !! Username already used, chose another one !!"
+
+    st.write(st.session_state.warning)
+
+
+def reco_widget():
     update_reco()
 
     col1, col2 = st.columns([4, 3])
@@ -159,12 +196,12 @@ if st.session_state.logged:
 
         if search != '':
             if search != st.session_state.search:
-                track_search(search)
+                API.track_search(search)
                 st.session_state.search = search
             with st.expander('Search result'):
                 for id, track in enumerate(st.session_state.search_result):
                     label = track.get('artist_name') + ' --->  ' + track.get('track_name')
-                    st.button(label=label, on_click=play, args=[track, id], key=label+'search')
+                    st.button(label=label, on_click=play, args=[track, id], key=label + '_search')
                     if id >= 10:
                         break
 
@@ -174,13 +211,31 @@ if st.session_state.logged:
             st.write('   1 - search for track')
             st.write('   2 - listen to one track')
             st.write('   3 - basic recommendation will be available')
+            st.write('   4 - personalised recommendation will be available model retrain')
 
         for id, track in enumerate(st.session_state.track_list):
             label = track.get('artist_name') + ' --->  ' + track.get('track_name')
-            st.button(label=label, on_click=play, args=[track, id, True], key=label+'reco')
+            st.button(label=label, on_click=play, args=[track, id, True], key=label + '_reco')
             if id >= 5:
                 break
 
-else:
-    st.title('Wellcome to PYTUNE! Please login!')
-    st.write("Pytune is an music recommender system based on collaborative filtering")
+
+if __name__ == "__main__":
+
+    with st.sidebar:
+        st.title('Pytune')
+
+        if not st.session_state.logged:
+            login_widget()
+
+        else:
+            st.write('User logged as: ', st.session_state.username)
+            if st.button('Log out'):
+                reset_session()
+                st.experimental_rerun()
+
+    if st.session_state.logged:
+        reco_widget()
+    else:
+        st.title('Wellcome to PYTUNE! Please login!')
+        st.write("Pytune is an music recommender system based on collaborative filtering")
